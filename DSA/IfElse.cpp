43355b734c891537ff99b4c8b773daf823b2360f
bhrataRitesh <ratohikumar@gmail.com>
2022-06-02 15:38:59 +0530

    if else example

--- DSA/IfElse.cpp
+++ DSA/IfElse.cpp
@@ -2,7 +2,13 @@
 using namespace std;
  
 int main() {
-cout << "here we are running if else program " <<endl;
-cout << "here I am testing this again " << endl;
+int a , b;
+cout<<"enter the value of a and b respectively:- \n ";
+cin >>a>>b;
+cout<<"greater number is : " ;
+if(a>b)
+cout<< a;
+else 
+cout<<b;
 return 0 ;
 }
