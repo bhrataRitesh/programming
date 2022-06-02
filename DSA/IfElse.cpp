9cd03fe48073e174b782ab01d9b7eb5337462044
bhrataRitesh <ratohikumar@gmail.com>
2022-06-02 15:44:14 +0530

    here we learnt about the use of cin.get()

--- DSA/IfElse.cpp
+++ DSA/IfElse.cpp
@@ -2,6 +2,8 @@
 using namespace std;
  
 int main() {
+    //understanding if else
+    /*
 int a , b;
 cout<<"enter the value of a and b respectively:- \n ";
 cin >>a>>b;
@@ -10,5 +12,12 @@ if(a>b)
 cout<< a;
 else 
 cout<<b;
+*/
+
+
+// LEts understand the use of cin.get() how its works
+int p ;
+ p =cin.get();
+ cout << p ;
 return 0 ;
 }
