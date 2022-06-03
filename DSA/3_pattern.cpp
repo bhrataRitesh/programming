39709047068775b400b8c85a57a4c6975aad1913
bhrataRitesh <ratohikumar@gmail.com>
2022-06-03 23:21:15 +0530

    till pattern 10

--- DSA/3_pattern.cpp
+++ DSA/3_pattern.cpp
@@ -2,6 +2,259 @@
 using namespace std;
  
 int main() {
+//1. pattern is 
+// 1 2 3 4 5 ....n
+// 1 2 3 4 5 ....n
+// int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//             cout<<j;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
 
+    //2. pattern
+    // n ... 3 2 1
+    // n ... 3 2 1
+    // int n;
+    // cin>>n;
+    // int i=1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<n-j+1;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+    //3. pattern 
+    // 1 2 3
+    //4 5 6
+    //7 8 9
+// int n;
+//     cin>>n;
+//     int i=1;
+//     int count =0;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//              count++;
+//             cout<<count;
+           
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+    // 4. pattern
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+
+    // int n;
+    // cin>>n;
+    // int i=1;
+   
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=i){
+            
+    //         cout<<"*";
+           
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // 5. pattern 
+    // 1
+    // 22
+    // 333
+    // 4444
+    // 55555
+    //   int n;
+    // cin>>n;
+    // int i=1;
+   
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=i){
+            
+    //         cout<<i;
+           
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+    
+    // 6. pattern 
+    //   int n;
+    // cin>>n;
+    // int i=1,count=1;
+   
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=i){
+            
+    //         cout<<count;
+    //        count++;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // 7. pattern 
+    // 1
+    // 23
+    // 345
+    // 4567
+    // 56789
+    //  int n;
+    // cin>>n;
+    // int i=1;
+   
+    // while(i<=n){
+    //     int j=1;
+    //    int count=i;
+    //     while(j<=i){
+            
+    //         cout<<count;
+    //        count++;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+    // or 
+    //  int n;
+    // cin>>n;
+    // int i=1;
+   
+    // while(i<=n){
+    //     int j=1;
+       
+    //     while(j<=i){
+            
+    //         cout<<i+j-1;
+           
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // 8. pattern 
+    // 1
+    // 21
+    // 321
+    // 4321
+    //  int n;
+    // cin>>n;
+    // int i=1;
+   
+    // while(i<=n){
+    //     int j=1;
+    //    int count=i;
+    //     while(j<=i){
+            
+    //         cout<<count;
+    //        count--;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+    // or 
+    //  int n;
+    // cin>>n;
+    // int i=1;
+   
+    // while(i<=n){
+    //     int j=1;
+       
+    //     while(j<=i){
+            
+    //         cout<<i-j+1;
+           
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // 9. pattern 
+    
+//      int n;
+//     cin>>n;
+//     int i=1;
+//    char c='A';
+//     while(i<=n){
+//         int j=1;
+       
+//         while(j<=n){
+            
+//             cout<<c<<" ";
+            
+           
+//             j++;
+//         }
+//         c++;
+//         cout<<endl;
+//         i++;
+//     }
+//or
+
+// int n;
+//     cin>>n;
+//     int i=1;
+   
+//     while(i<=n){
+//         int j=1;
+       
+//         while(j<=n){
+//             char c='A'+i-1;
+//             cout<<c<<" ";
+            
+           
+//             j++;
+//         }
+        
+//         cout<<endl;
+//         i++;
+//     }
+
+// 10. pattern
+// A B C
+// A B C
+// A B C
+
+int n;
+    cin>>n;
+    int i=1;
+   
+    while(i<=n){
+        int j=1;
+       
+        while(j<=n){
+            char c='A';
+            cout<<c<<" "; 
+            c=c+1;;
+            j++;
+        }
+        
+        cout<<endl;
+        i++;
+    }
 return 0 ;
 }
--- DSA/tempCodeRunnerFile.cpp
+++ DSA/tempCodeRunnerFile.cpp
@@ -1,8 +1,1 @@
-char a;
-cin>>a;
-if(a>='a' && a<='z')
-cout<<"lower case";
-else if(a>='A' && a<='Z')
-cout<<"upper case";
-else if(a>=48 && a<=57 )
-cout<<"numeric";
+ // c++;
