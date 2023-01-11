bc87bc01e1fd440caa6a35c0cde8d5da3f9be5f3
Ritesh <ratohikumar@gmail.com>
2023-01-12 02:30:55 +0530

    added jokes both hindi and english & linked them

--- Web Development/Bootcamp/Javascript/jokes/hindi/app1.js
+++ Web Development/Bootcamp/Javascript/jokes/hindi/app1.js
@@ -8,17 +8,19 @@ refresh.addEventListener("click", async (e) => {
   e.preventDefault();
   // alert("hello");
   // console.log("bro");
+  // header.innerText = "";
   header.innerText = "";
   //   const config = { headers: { Accept: "application/json" } };
   try {
     const addJokes = await axios.get(
       "https://hindi-jokes-api.onrender.com/jokes/1?api_key=aaf4bf5a02fe4f1c083ba14d4b3d"
     );
-    // const para = document.createElement("p");
+    console.log(addJokes.data.data[0].jokeContent);
+    const para = document.createElement("p");
 
-    // para.innerText = addJokes.data.jokeContent;
-    // header.append(para);
-    console.log(addJokes.data);
+    para.innerText = addJokes.data.data[0].jokeContent;
+    header.append(para);
+
     // console.log(para);
   } catch (e) {
     console.log("Error!");
